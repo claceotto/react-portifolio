ea2f5a3bd958670a88c9132e48fe1da10d96e0c6
claceotto <clatimponi@gmail.com>
2024-01-19 19:56:04 +0000

    Landing page with avatar, name and introduction

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -6,8 +6,11 @@ const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
+//https://i.pravatar.cc/150?img=7 
+
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
+
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,6 +18,22 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <Avatar name='Pete' size='lg' src='https://i.pravatar.cc/150?img=7' />
+    <VStack
+      spacing={2}
+      align='center'
+    >
+      <Heading as='h6' size='xs' pb={7}>
+        {greeting}
+      </Heading>
+      <Heading>
+        {bio1}
+      </Heading>
+      <Heading>
+        {bio2}
+      </Heading>
+    </VStack>
+
 
   </FullScreenSection>
 );
