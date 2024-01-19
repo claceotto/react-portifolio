5c91b773b2a9a7bfe2fd28928a66bbfb8cf67166
claceotto <clatimponi@gmail.com>
2024-01-19 19:56:17 +0000

    Comment added

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,6 +32,7 @@ const socials = [
   },
 ];
 
+//Creates the icons based on the const socials 
 const externalLinks = socials.map(social => {
   return (
     <a className='external-link' href="social.url">
