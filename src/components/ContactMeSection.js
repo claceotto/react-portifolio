21c722739b964720310da921e2ce65dc8fe9feb2
claceotto <clatimponi@gmail.com>
2024-01-23 20:44:28 +0000

    Made the form responsive

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -57,7 +57,7 @@ const LandingSection = () => {
       py={16}
       spacing={8}
     >
-      <VStack w="1024px" p={32} alignItems="flex-start">
+      <VStack w={[500, 600, 1024]} p={32} alignItems="flex-start">
         <Heading as="h1" id="contactme-section">
           Contact me
         </Heading>
