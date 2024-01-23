829a96821d99289dcaa4a2dc735c7c4ed827d589
claceotto <clatimponi@gmail.com>
2024-01-23 20:11:05 +0000

    Changed implementation to Formik  Hook and form now re-sets when submission is sucessuful

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -16,33 +16,39 @@ import * as Yup from 'yup';
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import { useAlertContext } from "../context/alertContext";
-import { isDOMComponent } from "react-dom/test-utils";
 
 const LandingSection = () => {
   const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
+  const { errors, touched, values, handleChange, handleSubmit, resetForm } = useFormik({
+    initialValues: {
+      firstName: '',
+      email: '',
+      type: 'hireMe',
+      comment: '',
+    },
+    onSubmit: (values, actions) => {
+      submit('http://localhost:3000/', values)
+      actions.setSubmitting(false)
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required('Required'),
+      email: Yup.string().email('Email must be valid').required('Required'),
+      type: Yup.string().required('Required'),
+      comment: Yup.string().required('Required'),
+    }),
+  });
+
   useEffect(() => {
     if (response) {
       onOpen(response.type, response.message)
+
+      if (response.type === 'success') {
+        resetForm()
+      }
     }
   }, [response]);
-
-  // const formik = useFormik({
-  //   initialValues: {
-  //     firstName: '',
-  //     email: '',
-  //     type: '',
-  //     comment: '',
-  //   },
-  // onSubmit: (values) => { submit('http://localhost:3000/', values) },
-  //   validationSchema: Yup.object({
-  //     firstName: Yup.string().required('Required'),
-  //     email: Yup.string().email('Invalid email').required('Required'),
-  //     type: Yup.string().required('Required'),
-  //     comment: Yup.string().required('Required'),
-  //   }),
-  // });
 
   return (
     <FullScreenSection
@@ -56,83 +62,59 @@ const LandingSection = () => {
           Contact me
         </Heading>
         <Box p={6} rounded="md" w="100%">
-          <Formik
-            initialValues={{
-              firstName: '',
-              email: '',
-              type: 'hireMe',
-              comment: '',
-            }}
-            onSubmit={(values, actions) => {
-              submit('http://localhost:3000/', values)
-              actions.setSubmitting(false)
-            }}
-            validationSchema={Yup.object({
-              firstName: Yup.string().required('Required'),
-              email: Yup.string().email('Email must be valid').required('Required'),
-              type: Yup.string().required('Required'),
-              comment: Yup.string().required('Required'),
-            })}
-          >
-            {({ handleSubmit, handleChange, errors, touched, values }) => (
-              <form onSubmit={(e) => { e.preventDefault(); handleSubmit(e) }}>
-                <VStack spacing={4}>
-                  <FormControl isInvalid={!!errors.firstName && touched.firstName}>
-                    <FormLabel htmlFor="firstName">Name</FormLabel>
-                    <Field
-                      as={Input}
-                      id="firstName"
-                      name="firstName"
-                      type='string'
-                      onChange={handleChange}
-                      value={values.firstName}
-                    />
-                    <FormErrorMessage>{errors.firstName}</FormErrorMessage>
-                  </FormControl>
-                  <FormControl isInvalid={!!errors.email && touched.email}>
-                    <FormLabel htmlFor="email">Email Address</FormLabel>
-                    <Field
-                      as={Input}
-                      id="email"
-                      name="email"
-                      type="email"
-                      onChange={handleChange}
-                      value={values.email}
-                    />
-                    <FormErrorMessage>{errors.email}</FormErrorMessage>
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                    <Select id="type" name="type" value={values.type} onChange={handleChange}>
-                      <option value="hireMe">Freelance project proposal</option>
-                      <option value="openSource">Open source consultancy session</option>
-                      <option value="other">Other</option>
-                    </Select>
-                  </FormControl>
-                  <FormControl isInvalid={!!errors.comment && touched.comment}>
-                    <FormLabel htmlFor="comment">Your message</FormLabel>
-                    <Field
-                      as={Textarea}
-                      id="comment"
-                      name="comment"
-                      height={250}
-                      onChange={handleChange}
-                      value={values.comment}
-                    />
-                    <FormErrorMessage>{errors.comment}</FormErrorMessage>
-                  </FormControl>
-                  <Button
-                    type="submit"
-                    colorScheme="purple"
-                    width="full"
-                    disabled={isLoading}
-                  >
-                    {isLoading ? 'Loading...' : 'Submit'}
-                  </Button>
-                </VStack>
-              </form>
-            )}
-          </Formik>
+          <form onSubmit={(e) => { e.preventDefault(); handleSubmit(e) }}>
+            <VStack spacing={4}>
+              <FormControl isInvalid={!!errors.firstName && touched.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  type='text'
+                  onChange={handleChange}
+                  value={values.firstName}
+                />
+                <FormErrorMessage>{errors.firstName}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.email && touched.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+                <FormErrorMessage>{errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select id="type" name="type" value={values.type} onChange={handleChange}>
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">Open source consultancy session</option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl isInvalid={!!errors.comment && touched.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  onChange={handleChange}
+                  value={values.comment}
+                />
+                <FormErrorMessage>{errors.comment}</FormErrorMessage>
+              </FormControl>
+              <Button
+                type="submit"
+                colorScheme="purple"
+                width="full"
+                disabled={isLoading}
+              >
+                {isLoading ? 'Loading...' : 'Submit'}
+              </Button>
+            </VStack>
+          </form>
         </Box>
       </VStack>
     </FullScreenSection >
