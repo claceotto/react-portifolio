c7bee970ba4969761a491189eb3683ae594f734c
claceotto <clatimponi@gmail.com>
2024-01-31 19:40:18 +0000

    Cards are now bigger

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,54 +1,34 @@
-import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-
   return (
-    <Box
-      maxW='sm'
-      backgroundColor='#FFFFFF'
-      borderRadius='10'
+    <VStack
+      color="black"
+      backgroundColor="white"
+      borderRadius="xl"
     >
-      <Box
-      >
-        <Image
-          src={imageSrc}
-          alt='Project Image'
-          borderRadius='10'
-
-        />
-      </Box>
-      <Heading
-        color='black'
-        p={2}
-        paddingTop={4}
-        as='h5' size='sm'
-      >
-        {title}
-      </Heading>
-      <Text
-        color='black'
-        p={2}
-        fontSize='xs'
-      >
-        {description}
-      </Text>
-      <Text
-        color='black'
-        p={2}
-        fontWeight="bold"
-        fontSize='xs'
-      >
-        See More <FontAwesomeIcon icon={faArrowRight} />
-      </Text>
-    </Box>
-  )
-    ;
+      <Image
+        src={imageSrc}
+        borderRadius="xl" />
+      <VStack spacing={5} p={5} alignItems="flex-start">
+        <HStack justifyContent="space-between" alignItems="center">
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+        </HStack>
+        <Text fontSize="lg" color="grey">
+          {description}
+        </Text>
+        <HStack>
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
